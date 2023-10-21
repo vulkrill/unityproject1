1058a9fd7cf3b0fd75865f8b9a2c15ee08fd0212
vulkrill <92627315+vulkrill@users.noreply.github.com>
2023-10-21 20:16:40 +0300

    task#1

---
+++ My project/Assets/script.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public GameObject Object;
+
+    public void Press()
+    {
+        if (Object.active == true)
+        {
+            Debug.Log("Object deacivated");
+            Object.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Object acivated");
+            Object.SetActive(true);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
